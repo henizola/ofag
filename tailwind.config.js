0037146d2742dae22a00ffcc68aa4fcaca6a7dfd
Henok Zelalem <henok.zelalem@flowscapesolutions.com>
2025-06-08 11:45:38 +0300

    initiial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        mainblue: '#2B4D7B',
+      },
+    },
+  },
+  plugins: [],
+} 
