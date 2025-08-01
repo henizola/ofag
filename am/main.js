1be06f866f21fdb9def64028ac1231eb8815afd7
Henok Zelalem <henok.zelalem@flowscapesolutions.com>
2025-08-01 17:28:13 +0300

    update

--- am/main.js
+++ am/main.js
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     <li>
                         <button class="flex items-center justify-between w-full text-mainblue text-lg font-medium mobile-sub-toggle">አገልግሎቶች<svg class="h-4 w-4 transition-transform" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"/></svg></button>
                         <ul class="mt-2 pl-4 space-y-2 hidden">
-                            <li><a href="services.html#business-services" class="block text-mainblue">የንግድ አገልግሎቶች</a></li>
+                            <li><a href="services.html#business-services" class="block text-mainblue">የኦዲት አገልግሎቶች</a></li>
                             <li><a href="services.html#online-services" class="block text-mainblue">የመስመር ላይ አገልግሎቶች</a></li>
                             <li><a href="services.html#service-guidelines" class="block text-mainblue">የአገልግሎት መመሪያዎች</a></li>
                             <li><a href="services.html#faqs" class="block text-mainblue">ተደጋጋሚ ጥያቄዎች</a></li>
